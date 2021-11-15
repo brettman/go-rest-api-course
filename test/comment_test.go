d6ad2bf7c59898f6f4f07bc01091b106460279a0
brettman <brettman@gmail.com>
2021-11-15 19:05:19 +0200

    added some e2e tests

---
+++ test/comment_test.go
@@ -0,0 +1,25 @@
+// +build e2e
+
+package test
+
+
+import (
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const(
+	BASE_URL = "http://localhost:8080"
+)
+
+func TestGetComments(t *testing.T){
+	client := resty.New()
+	resp, err := client.R().Get(BASE_URL + "/api/comment")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
+

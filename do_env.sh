ca02f74eb06f013731f43b1e40fea9fc897183c1
Brett Hardman <brettman@users.noreply.github.com>
2021-11-16 21:07:21 +0200

    Delete do_env.sh

--- do_env.sh
+++
@@ -1,5 +0,0 @@
-export DB_USERNAME=doadmin
-export DB_PASSWORD=h2xAFRadJfMF2tNm
-export DB_HOST=db-postgresql-fra1-13999-do-user-10037102-0.b.db.ondigitalocean.com
-export DB_PORT=25060
-export DB_TABLE=defaultdb
